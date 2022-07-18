dc99b77f996b38a7ef5b1c70f1a4bb29fedc7cbd
ansk1d <anasalkhaled@aol.com>
2022-07-18 11:51:31 +0300

    hello Java

---
+++ Firstjava.java
@@ -0,0 +1,7 @@
+public class Firstjava {
+    public static void main(String[] args) {
+        System.out.println( " My name is Anas Alkhaled ");
+        System.out.println( " I am 25 years old ");
+        System.out.println( " My hometown is Abha, SA ");
+    }
+}
